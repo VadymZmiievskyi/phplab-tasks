c3749e90aef8145a3787b286fab92764122729d3
vadym <12vadym098696@gmail.com>
2020-07-14 14:22:46 +0300

    -

--- src/01-basics.php
+++ src/01-basics.php
@@ -10,9 +10,39 @@
  * @return string
  * @throws InvalidArgumentException
  */
-function getMinuteQuarter(int $minute)
+function getMinuteQuarter($minute)
 {
+
+
+    if ($minute >= 0 and $minute <= 14) {
+        return " first";
+    }
+
+    if ($minute <= 15 and $minute >= 30) {
+        return "second";
+    }
+
+    if ($minute <= 31 and $minute >= 45) {
+        return"third";
+    }
+
+
+    if ($minute >= 46 and $minute <= 59) {
+        return"fourth";
+    }
+
+    if($minute > 60){
+        throw new InvalidArgumentException("must be < than 60");
+    }
 }
+
+try{
+    echo getMinuteQuarter(5);
+
+}catch (\InvalidArgumentException $e){
+    echo $e->getMessage();
+}
+
 
 /**
  * The $year variable contains a year (i.e. 1995 or 2020 etc).
@@ -24,9 +54,34 @@ function getMinuteQuarter(int $minute)
  * @param  int  $year
  * @return boolean
  * @throws InvalidArgumentException
- */
-function isLeapYear(int $year)
-{
+*/
+
+function isLeapYear($year){
+
+    if($year < 1900){
+        throw new InvalidArgumentException("must be > than 1990");
+    }elseif($year == 2000){
+        throw new InvalidArgumentException("true");
+    }elseif($year % 4 !=0) {
+        return"false";
+    }elseif($year % 100 !=0){
+        return"true";
+
+
+    }elseif($year / 400 !=0){
+        return"false";
+
+    }
+    else{
+        return"true";
+    }
+}
+
+try{
+    echo isLeapYear(2000);
+
+}catch (\InvalidArgumentException $a){
+    echo $a->getMessage();
 }
 
 /**
@@ -39,7 +94,43 @@ function isLeapYear(int $year)
  * @param  string  $input
  * @return boolean
  * @throws InvalidArgumentException
- */
-function isSumEqual(string $input)
-{
+*/
+function isSumEqual(string $input) {
+
+    if(mb_strlen($input) > 6) {
+        throw new InvalidArgumentException("should be 6 values");
+
+    }
+
+    $arr1 = str_split($input);
+
+    $val1 = 0;
+    $val2 = 0;
+    $i = 0;
+
+    foreach($arr1 as $key => $value) {
+        if ($i < 3) {
+            $val1 += $value;
+        }
+        else {
+            $val2 += $value;
+        }
+
+        $i++;
+    }
+
+
+    if($val1 == $val2) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+try{
+    var_dump (isSumEqual("123456"));
+
+}catch (\InvalidArgumentException $e){
+    echo $e->getMessage();
+}
+
